16613d6b06d7a0cf937ae39c9b1b74b77612a4be
Garth Groshans II, PhD <garthgroshansii@gmail.com>
2024-10-16 15:00:00 -0400

    initial commit; create albumrequest model and controller template

---
+++ RandomAlbumAPI/Controllers/AlbumsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RandomAlbumAPI.Models;
+
+namespace RandomAlbumAPI.Controllers
+{
+
+    [Route("api/Album")]
+    [ApiController]
+    public class AlbumsController : ControllerBase
+    {
+
+        public IActionResult CreateAlbum([FromBody] AlbumRequest albumRequest)
+        {
+            Console.WriteLine("FIRED");
+            return Ok();
+        }
+         
+
+    }
+}
---
+++ RandomAlbumAPI/Models/AlbumRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace RandomAlbumAPI.Models
+{
+    public class AlbumRequest
+    {
+        public string ArtistName { get; set; }
+        public string AlbumName { get; set; }
+    }
+}
