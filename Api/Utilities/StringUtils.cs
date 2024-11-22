d7b533a55d31ab059b99e33ca2e9046e4617f2c4
Garth Groshans Jr, PhD <garthgroshansii@gmail.com>
2024-11-22 12:58:23 -0500

    add string utils with function to convert to snake case

---
+++ Api/Utilities/StringUtils.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+
+namespace Api.Utilities
+{
+    public class StringUtils
+    {
+        public static string ConvertToSnakeCase(string input)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+
+            var stringBuilder = new StringBuilder(input.Length + (input.Length / 2));
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (char.IsUpper(c))
+                {
+                    if (i > 0) stringBuilder.Append('_');
+                    stringBuilder.Append(char.ToLower(c));
+                }
+                else
+                {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
