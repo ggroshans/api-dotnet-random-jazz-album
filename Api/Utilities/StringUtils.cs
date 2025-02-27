b785ca435a2385aa20ad3e13e79b9eea5af7c0a8
Garth Groshans Jr, PhD <garthgroshansii@gmail.com>
2025-02-26 21:30:05 -0500

    create capitalize / format string util

--- Api/Utilities/StringUtils.cs
+++ Api/Utilities/StringUtils.cs
@@ -26,5 +26,41 @@ namespace Api.Utilities
 
             return stringBuilder.ToString();
         }
+
+        public static string CapitalizeAndFormat(string input)
+        {
+            string formattedString = "";
+
+            if (input.Contains('-'))
+            {
+                var splitString = input.Split('-');
+                List<string> capitalizedWords = new List<string>();
+                foreach (var item in splitString)
+                {
+                    var capitalizedWord = char.ToUpper(item[0]) + item.Substring(1).ToLower();
+                    capitalizedWords.Add(capitalizedWord);
+                }
+                formattedString = string.Join(" ", capitalizedWords);
+            }
+
+            else if (input.Contains(' '))
+            {
+                var splitString = input.Split(' ');
+                List<string> capitalizedWords = new List<string>();
+                foreach (var item in splitString)
+                {
+                    var capitalizedWord = char.ToUpper(item[0]) + item.Substring(1).ToLower();
+                    capitalizedWords.Add(capitalizedWord);
+                }
+                formattedString = string.Join(" ", capitalizedWords);
+            }
+
+            else if (!input.Contains('-') && !input.Contains(' '))
+            {
+                formattedString = char.ToUpper(input[0]) + input.Substring(1).ToLower();
+            }
+
+            return formattedString;
+        }
     }
 }
