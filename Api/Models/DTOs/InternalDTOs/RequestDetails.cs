92254f5b5a0df73a5a249f90f800198a086c3ba8
Garth Groshans Jr, PhD <garthgroshansii@gmail.com>
2025-04-15 15:26:00 -0400

    move to internalDTOs folder

---
+++ Api/Models/DTOs/InternalDTOs/RequestDetails.cs
@@ -0,0 +1,11 @@
+﻿namespace Api.Models.DTOs.InternalDTOs
+{
+    public class RequestDetails
+    {
+        public string? PrimaryArtistName { get; set; }
+        public int? ArtistCount { get; set; } = 0;
+        public int? AlbumCount { get; set; } = 0;
+        public int? SubgenreCount { get; set; } = 0;
+        public int? MoodCount { get; set; } = 0;
+    }
+}
